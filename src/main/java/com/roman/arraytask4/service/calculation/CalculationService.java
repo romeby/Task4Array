07834261fbc8cde503c2b52858ddea722418aa60
romeby <roman.zhmailik@gmail.com>
2023-02-06 18:31:05 +0300

    06.02

--- src/main/java/com/roman/arraytask4/service/calculation/CalculationService.java
+++ src/main/java/com/roman/arraytask4/service/calculation/CalculationService.java
@@ -6,6 +6,6 @@ import com.roman.arraytask4.util.PositivNegative;
 public interface CalculationService {
 
     public int sumOfElements(ArrayClass arrayClass);
-    public int average(ArrayClass arrayClass);
-    int countPositiveNegative(PositivNegative sign, ArrayClass arrayClass);
+    public double average(ArrayClass arrayClass);
+    public int countPositiveNegative(PositivNegative sign, ArrayClass arrayClass);
 }
