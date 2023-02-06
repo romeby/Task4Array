10e7e5faba4f35f5f56d6557b9015eccbc293731
romeby <roman.zhmailik@gmail.com>
2023-02-06 18:26:19 +0300

    06.02

--- src/main/java/com/roman/arraytask4/main/ArrayMain.java
+++ src/main/java/com/roman/arraytask4/main/ArrayMain.java
@@ -3,6 +3,7 @@ package com.roman.arraytask4.main;
 import com.roman.arraytask4.entity.ArrayClass;
 import com.roman.arraytask4.service.calculation.impl.CalculationServiceImpl;
 import com.roman.arraytask4.util.PositivNegative;
+import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -13,8 +14,8 @@ public class ArrayMain {
         int[] array = {2, 18, 92, -6};
         ArrayClass arrayClass = new ArrayClass(array);
         CalculationServiceImpl calculationService = new CalculationServiceImpl();
-        calculationService.sumOfElements(arrayClass);
         calculationService.average(arrayClass);
+        calculationService.sumOfElements(arrayClass);
         calculationService.countPositiveNegative(PositivNegative.NEGATIVE, arrayClass);
     }
 
